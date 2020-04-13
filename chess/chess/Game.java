b85890347c776dd2d76166daea05b70ffa9d48b0
DomovoiG <48762728+DomovoiG@users.noreply.github.com>
2020-04-13 02:23:23 -0700

    set-up
    This is the first commit, includes basic game loop and window set up.

---
+++ chess/chess/Game.java
@@ -0,0 +1,97 @@
+package chess;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = 1550691097823471818L;
+	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12*9;
+	
+	private Thread thread;
+	
+	private boolean running = false;
+	
+	public Game()
+	{
+		new Window(WIDTH, HEIGHT, "gamestuff",this);
+	}
+	
+	public synchronized void start()
+	{
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+		
+	}
+	
+	public synchronized void stop()
+	{
+		try
+		{
+			thread.join();
+			running=false;
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	public void run()
+	{
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running)
+		{
+			tick();
+			delta--;
+		
+			if(running)
+				render();
+			
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000)
+			{
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		//stop();
+	}
+	
+	private void tick()
+	{
+		
+	}
+	
+	private void render()
+	{
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null)
+		{
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0,0,WIDTH,HEIGHT);
+		
+		g.dispose();
+		bs.show();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new Game();
+	}
+
+}
